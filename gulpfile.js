42c9fa567ed2becd48aaa5eba45fd166ffa33cf0
Luke Noel-Storr <lukens@me.com>
2016-02-07 16:32:52 +0000

    added gulp task to deploy to webtask.io from local build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var exec = require('gulp-exec')
+
+var srcs = ['init.js', 'src/*.js']
 
 gulp.task('default', function() {
-  return gulp.src(['init.js', 'src/*.js'])
+
+  return gulp.src(srcs)
     .pipe(concat('spongebot-webtask.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist'))
+
+})
+
+gulp.task('deploy-from-local', ['default'], function() {
+
+  return gulp.src('dist/spongebot-webtask.js')
+    .pipe(exec('echo "deployed to:" `wt create <%= file.path %> -n spongebot`'))
+    .pipe(exec.reporter());
+
+})
