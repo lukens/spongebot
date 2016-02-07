a3488290931a35e0a085c3f41bf60621815b7ed2
Luke Noel-Storr <lukens@me.com>
2016-02-07 20:03:41 +0000

    added help

--- init.js
+++ init.js
@@ -28,6 +28,24 @@ module.exports = function() {
       SpongeBot.githubIssues.showIssue(
         message, match[4], match[5], match[7], match[2] === "everyone")
     })
+    // help
+    .when(/^help$/i, function(message) {
+      message.respond(
+        "*Help help:* `[I'm optional]` `(choose me|or me)` "
+        + "`[choose me|or me|or neither]` `<put something here>`\n"
+        + "\n"
+        + "*Available commands:*\n"
+        + "_Silliness:_\n"
+        + "`/spongebot hello` - I politely reply\n"
+        + "`/spongebot echo <some text>` - I say whatever you typed after 'echo'\n"
+        + "`/spongebot insult me [publicly]` - I insult you (infront of everyone, if you choose)\n"
+        + "\n"
+        + "_Useful Stuff:_\n"
+        + "`/spongebot show [me|everyone] [owner] <repo> (issue |#)<number>`"
+        + " - I show you (or everyone) details of the github issue in the given repository\n"
+        + "\n"
+        + "(sorry, that's all for now)")
+    })
     // return the listen method
     .listen
 
